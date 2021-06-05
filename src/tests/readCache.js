8cf187f4a78540d19ef4e538e91e578d042f3d40
Tobias Schmücker <schmuecker.tobias@gmail.com>
2021-06-05 15:27:57 +0000

    Update cache path

--- src/tests/readCache.js
+++ src/tests/readCache.js
@@ -2,10 +2,10 @@ const path = require("path");
 var flatCache = require("flat-cache");
 
 // eslint-disable-next-line no-undef
-const cachePath = path.join(__dirname, "../cache");
+const cachePath = path.join(__dirname, "../../cache");
 
 const crawlerCache = flatCache.load("crawlers", cachePath);
-console.log("Crawler in cache", crawlerCache.all());
+console.log("Crawler", Object.keys(crawlerCache.all()));
 
 const notifyCache = flatCache.load("notify", cachePath);
-console.log("Notifications in cache", notifyCache.all());
+console.log("Notifications", notifyCache.all());
